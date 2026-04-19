7e292a25cd7d8fca68e301d97bcd80a853ba7bed
onii <oniisasaki@gmail.com>
2026-04-19 14:16:57 +0700

    new brust mech add

--- app/Http/Controllers/ForecastController.php
+++ app/Http/Controllers/ForecastController.php
@@ -324,7 +324,7 @@ class ForecastController extends Controller
         putenv('USER=takemi');
         putenv('SUDO_ASKPASS=/bin/false');
 
-        $cmd = "sudo -n -H python3.9 {$script} > {$logFile} 2>&1 & echo $!";
+        $cmd = "echo 'Arius123.' | su -c 'python3.9 {$script} > {$logFile} 2>&1 & echo \$!' root";
         $descriptors = [
             0 => ['file', '/dev/null', 'r'],
             1 => ['file', $logFile, 'w'],
